66fbcfe7fcaf917f9b31057510e9f942ec03108d
LFOOT <lewis_foot@next.co.uk>
2024-08-16 16:07:14 +0100

    feat: added controller

---
+++ ControllerVsMinimalAPI/ControllerAPI/Commands/CreateWeatherCommand.cs
@@ -0,0 +1,5 @@
+﻿using MediatR;
+
+namespace ControllerAPI.Commands;
+
+public record CreateWeatherCommand(int Temperature) : IRequest;
---
+++ ControllerVsMinimalAPI/ControllerAPI/Controllers/WeatherController.cs
@@ -0,0 +1,26 @@
+﻿using ControllerAPI.Commands;
+using MediatR;
+using Microsoft.AspNetCore.Mvc;
+
+namespace ControllerAPI.Controllers;
+
+[ApiController]
+[Route("api/weather")]
+public class WeatherController : ControllerBase
+{
+    private readonly IMediator
+        _mediator;
+
+    public WeatherController(IMediator mediator)
+    {
+        _mediator = mediator;
+    }
+
+    [HttpPost]
+    public async Task<IActionResult> CreateWeather([FromQuery] int temperature)
+    {
+        await _mediator.Send(new CreateWeatherCommand(temperature));
+
+        return NoContent();
+    }
+}
